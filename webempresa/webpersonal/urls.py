b7f60ce0ae24f1980d68a596b3d5995b82d3faff
josean9 <114728402+josean9@users.noreply.github.com>
2024-11-22 18:28:38 +0000

    wfe

--- webempresa/webpersonal/urls.py
+++ webempresa/webpersonal/urls.py
@@ -14,19 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
-
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 urlpatterns = [
- path('', views.home, name="home"),
- path('about/', views.about, name="about"),
- path('services/', views.services, name="services"),
- path('store/', views.store, name="store"),
- path('contact/', views.contact, name="contact"),
- path('blog/', views.blog, name="blog"),
- path('sample/', views.sample, name="sample"),
+ path('core/', include('core.urls')),
+ path('admin/', admin.site.urls),
+ path('', views.home, name='home'),
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
