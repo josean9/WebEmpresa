ae253d6058baf96218353d9bdd11ec93338f2783
josean9 <114728402+josean9@users.noreply.github.com>
2024-11-22 18:17:56 +0000

    commit

--- webempresa/webpersonal/urls.py
+++ webempresa/webpersonal/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+ path('', views.home, name="home"),
+ path('about/', views.about, name="about"),
+ path('services/', views.services, name="services"),
+ path('store/', views.store, name="store"),
+ path('contact/', views.contact, name="contact"),
+ path('blog/', views.blog, name="blog"),
+ path('sample/', views.sample, name="sample"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
