8e1d3550481e2789bd33e4ad58e61d0739733864
josean9 <114728402+josean9@users.noreply.github.com>
2024-11-24 15:32:47 +0000

    blog

--- webempresa/services/models.py
+++ webempresa/services/models.py
@@ -12,9 +12,9 @@ class Service(models.Model):
     verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True,
     verbose_name="Fecha de edición")
-class Meta:
-      verbose_name = "servicio"
-      verbose_name_plural = "servicios"
-      ordering = ['-created']
-      def __str__(self):
-         return self.title
+    class Meta:
+         verbose_name = "servicio"
+         verbose_name_plural = "servicios"
+         ordering = ['-created']
+         def __str__(self):
+            return self.title
