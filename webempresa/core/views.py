26bc39dca4eb2c21c462cb26064732f1054295dc
josean9 <114728402+josean9@users.noreply.github.com>
2024-11-24 14:16:23 +0000

    base.html

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render, HttpResponse
-def home(request):
- return HttpResponse("Inicio")
-def about(request):
- return HttpResponse("Historia")
-def services(request):
- return HttpResponse("Servicios")
-def store(request):
- return HttpResponse("Visítanos")
-def contact(request):
- return HttpResponse("Contacto")
-def blog(request):
- return HttpResponse("Blog")
-def sample(request):
- return HttpResponse("Sample")
+from django.shortcuts import render
 def home(request):
  return render(request, "core/home.html")
+def about(request):
+ return render(request, "core/about.html")
+def services(request):
+ return render(request, "core/services.html")
+def store(request):
+ return render(request, "core/store.html")
+def contact(request):
+ return render(request, "core/contact.html")
+def blog(request):
+ return render(request, "core/blog.html")
+def sample(request):
+ return render(request, "core/sample.html")
