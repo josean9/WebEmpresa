c63751bafab65850c01063e6750ed64ac328bed9
josean9 <114728402+josean9@users.noreply.github.com>
2024-11-24 14:43:31 +0000

    aqui

--- webempresa/services/admin.py
+++ webempresa/services/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Service
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+admin.site.register(Service, ServiceAdmin)
--- webempresa/webpersonal/settings.py
+++ webempresa/webpersonal/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'services.apps.ServicesConfig',
+
 ]
 
 MIDDLEWARE = [
