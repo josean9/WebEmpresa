2cd55205b10d860d703693c8dcbcc23548aeefe0
josean9 <114728402+josean9@users.noreply.github.com>
2024-11-24 15:36:05 +0000

    commit

--- webempresa/core/urls.py
+++ webempresa/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-"""urlpatterns = [
+urlpatterns = [
  path('', views.home, name="home"),
  path('about/', views.about, name="about"),
  path('services/', views.services, name="services"),
@@ -8,11 +8,11 @@ from . import views
  path('contact/', views.contact, name="contact"),
  path('blog/', views.blog, name="blog"),
  path('sample/', views.sample, name="sample"),
-]"""
-urlpatterns = [
+]
+"""urlpatterns = [
  path('', include('core.urls')),
  path('blog/', include('blog.urls')),
  path('services/', include('services.urls')),
  path('admin/', admin.site.urls),
 ]
-
+"""
