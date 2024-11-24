b8c9de737b28bbed38fe6fd07571455dc738a84f
josean9 <114728402+josean9@users.noreply.github.com>
2024-11-24 15:27:13 +0000

    srevices.html

--- webempresa/core/urls.py
+++ webempresa/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-urlpatterns = [
+"""urlpatterns = [
  path('', views.home, name="home"),
  path('about/', views.about, name="about"),
  path('services/', views.services, name="services"),
@@ -8,5 +8,11 @@ urlpatterns = [
  path('contact/', views.contact, name="contact"),
  path('blog/', views.blog, name="blog"),
  path('sample/', views.sample, name="sample"),
+]"""
+urlpatterns = [
+ path('', include('core.urls')),
+ path('blog/', include('blog.urls')),
+ path('services/', include('services.urls')),
+ path('admin/', admin.site.urls),
 ]
 
--- webempresa/services/views.py
+++ webempresa/services/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Service
+def services(request):
+ services = Service.objects.all()
+ return render(request, "services/services.html",
+{'services':services})
